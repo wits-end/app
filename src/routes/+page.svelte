580539f721cee5ffc0da7a0742212bdedb9b8329
Tim Whitaker <tmwhtkr@gmail.com>
2024-07-01 11:20:04 -0700

    Add sidebar ad code

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -330,6 +330,23 @@
 
 			<div class="ads">
 				<h3 class="minion">Ads</h3>
+				<script
+					async
+					src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1498186405228029"
+					crossorigin="anonymous"
+				></script>
+				<!-- Sidebar -->
+				<ins
+					class="adsbygoogle"
+					style="display:block"
+					data-ad-client="ca-pub-1498186405228029"
+					data-ad-slot="3897793383"
+					data-ad-format="auto"
+					data-full-width-responsive="true"
+				></ins>
+				<script>
+					(adsbygoogle = window.adsbygoogle || []).push({});
+				</script>
 			</div>
 		</div>
 	</div>
