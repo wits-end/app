ad003322c4b21bb382c65c6c27ccd27b812ed241
Tim Whitaker <tmwhtkr@gmail.com>
2024-07-28 11:05:07 -0700

    Add newsletter form

--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -1,5 +1,6 @@
 import type { LayoutServerLoad } from './$types'
 
+
 export const load: LayoutServerLoad = async ({ locals: { safeGetSession } }) => {
     const { session, user } = await safeGetSession()
 
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,6 +2,7 @@
 	import Logo from '$lib/components/logo.svelte';
 	import { goto, invalidate } from '$app/navigation';
 	import { onMount } from 'svelte';
+	import { enhance } from '$app/forms';
 
 	export let data;
 	$: ({ session, supabase } = data);
@@ -58,29 +59,31 @@
 	<div class="wrapper">
 		<div class="newsletter">
 			<h2>Newsletter</h2>
-			<div class="input-group">
-				<div class="field-group">
-					<div class="icon">
-						<svg
-							aria-hidden="true"
-							data-prefix="fas"
-							data-icon="search"
-							class="svg-inline--fa fa-envelope fa-w-16"
-							role="img"
-							xmlns="http://www.w3.org/2000/svg"
-							viewBox="0 0 512 512"
-						>
-							<path
-								fill="currentColor"
-								d="M64 112c-8.8 0-16 7.2-16 16l0 22.1L220.5 291.7c20.7 17 50.4 17 71.1 0L464 150.1l0-22.1c0-8.8-7.2-16-16-16L64 112zM48 212.2L48 384c0 8.8 7.2 16 16 16l384 0c8.8 0 16-7.2 16-16l0-171.8L322 328.8c-38.4 31.5-93.7 31.5-132 0L48 212.2zM0 128C0 92.7 28.7 64 64 64l384 0c35.3 0 64 28.7 64 64l0 256c0 35.3-28.7 64-64 64L64 448c-35.3 0-64-28.7-64-64L0 128z"
+			<form action="/newsletter/?/addNewsletterEmail" method="POST" use:enhance>
+				<div class="input-group">
+					<div class="field-group">
+						<div class="icon">
+							<svg
+								aria-hidden="true"
+								data-prefix="fas"
+								data-icon="search"
+								class="svg-inline--fa fa-envelope fa-w-16"
+								role="img"
+								xmlns="http://www.w3.org/2000/svg"
+								viewBox="0 0 512 512"
 							>
-							</path></svg
-						>
+								<path
+									fill="currentColor"
+									d="M64 112c-8.8 0-16 7.2-16 16l0 22.1L220.5 291.7c20.7 17 50.4 17 71.1 0L464 150.1l0-22.1c0-8.8-7.2-16-16-16L64 112zM48 212.2L48 384c0 8.8 7.2 16 16 16l384 0c8.8 0 16-7.2 16-16l0-171.8L322 328.8c-38.4 31.5-93.7 31.5-132 0L48 212.2zM0 128C0 92.7 28.7 64 64 64l384 0c35.3 0 64 28.7 64 64l0 256c0 35.3-28.7 64-64 64L64 448c-35.3 0-64-28.7-64-64L0 128z"
+								>
+								</path></svg
+							>
+						</div>
+						<input type="email" name="email" placeholder="email@example.com" />
 					</div>
-					<input type="email" placeholder="email@example.com" />
+					<button type="submit">subscribe</button>
 				</div>
-				<button>subscribe</button>
-			</div>
+			</form>
 		</div>
 		<!-- <div class="column">
 			<h2>Site</h2>
